126e180b33346a6706a87acdec068a78111c9792
MonikaToruńska <torgosia@wp.pl>
2020-09-24 21:34:58 +0200

    --add button --remove divs from HTML and create in JS --shuffle function --createSquare function --startGame function

--- memory.js
+++ memory.js
@@ -1,8 +1,7 @@
-const cards = document.querySelectorAll('.memory-game .memory-card')
-cards.forEach(card => card.addEventListener("click", onCardClicked))
-
 let clickedCard = false
 let firstMatch, secondMatch;
+const colors = ["mint", "pink", "grey", "green", "blue", "graphite", "navy", "red"]
+
 
 function onCardClicked(event){
   console.log(event)
@@ -20,6 +19,7 @@ function onCardClicked(event){
 
 function checkForMatch(first, second) {
   if(first.dataset.color === second.dataset.color) {
+    return alert("NICE!");
     first.removeEventListener("click", onCardClicked);
     second.removeEventListener("click", onCardClicked);
   } else {
@@ -33,3 +33,50 @@ function clearMatches(first, second) {
     second.classList.add("color-hidden");
   }, 1500)
 }
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+};
+
+const game = document.querySelector(".memory-game");
+function startGame() {
+  game.innerHTML = "";
+
+  const squaresColors = [...colors, ...colors];
+  const shuffleSquareColors = shuffle(squaresColors);
+  shuffleSquareColors.forEach((color) => {
+    game.appendChild(createSquare(color));
+  })
+
+  const cards = document.querySelectorAll('.memory-game .memory-card')
+  cards.forEach(card => card.addEventListener("click", onCardClicked))
+
+}
+window.onload = () => startGame();
+
+
+let button = document.createElement('button');
+button.innerHTML = "Play again";
+document.body.appendChild(button);
+
+button.addEventListener("click", startGame);
+
+function createSquare(color) {
+  const square = document.createElement('div');
+  square.classList.add('memory-card', 'color-hidden', color);
+  square.dataset.color = color;
+
+  return square;
+}
+
+
+
+
+
+
